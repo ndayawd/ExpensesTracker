7af6e49e2322bf1ff359c2aac328d82c15751428
Chester00000 <chestertu2009@gmail.com>
2025-01-31 14:34:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 
+expenses = {}
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 clear()
 
 print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
-
 def main():
+    
+    
     action = input("Select an action (1/2/3/4): ")
     if action == '1':
         clear()
@@ -26,11 +29,39 @@ def main():
         main()
 
 def add_expenses():
+    print("")
 
 def view_expenses():
+    if len(expenses) < 1:
+        print("Expense Tracker - Current Expenses:\nNo expenses found!\n") 
 
-def calculate_total_expenses()
+        user_input = input("Press enter to Exit: ")
+        if user_input.lower() == 'exit':
+            clear()
+            return
+        else:
+            clear()
+            print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
+            main()
+            return
 
-def delete_expenses()
+    else:
+        print("Expense Tracker - Current Expenses:\n")
+        print(expenses)
 
+        user_input = input("Press enter to Exit: ")
+        if user_input.lower() == 'exit':
+            clear()
+            return
+        else:
+            clear()
+            print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
+            main()
+            return
+
+def calculate_total_expenses():
+    print("")
+
+def delete_expenses():
+    print("")
 main()
