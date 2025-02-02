671b0c1f34d3cafb12e4297634b43f97f462f045
deaconlam <127363839+deaconlam@users.noreply.github.com>
2025-02-01 18:47:08 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ExpensesTracker
+# Expenses Tracker
+a Python program that manages and tracks expenses
+
+# Members
+Andy Yung - handles adding expenses
+Chester Tu - implements the calculation of total expenses
+Deacon Lam - works on filtering or deleting expenses
--- main.py
+++ main.py
@@ -2,90 +2,59 @@ import os
 from datetime import datetime 
 import time
 
+expenses = {"Date": [], "Category": [], "Name": [], "Amount": []}
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 clear()
 
-expenses = {"Date": [], "Category": [], "Name": [], "Amount": []}  
-
-print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
-def main():
-    action = input("Select an action (1/2/3/4): ")
+def main(message):
+    print(f"Expense Tracker\n\nNo Expenses\n\nActions\n1. Add Expenses\n2. Delete Expenses\n{message}" if expenses["Date"] == [] else f"Expense Tracker\n\n{expenses}\n\nActions\n1. Add Expenses\n2. Delete Expenses\n{message}") 
+    action = input("Select an action (1/2): ")
     if action == '1':
         clear()
         add_expenses()
     elif action == '2':
         clear()
-        view_expenses()
-    elif action == '3':
-        clear()
-        calculate_total_expenses()
-    elif action == '4':
-        clear()
         delete_expenses()
     else:
         clear()
-        print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\nInvalid action, please try again.\n")
-        main()
-        
-def add_expenses(): 
-        category = input("Enter a category for your expense: ")
-        name = input("Enter a name for your expense: ")
-        while True:
-            try:
-                amount = input("Enter an amount: ")
-                amount = "{:.2f}".format(float(amount))
-                break
-            except Exception as e:
-                clear()
-                print(e)
-                print("please enter a valid number")
-                time.sleep(1)
-                clear()
-        while True:
-            date_input = input("Enter a date (in YYYY-MM-DD format): ")
-            try:
-                date_object = datetime.strptime(date_input, "%Y-%m-%d")
-                date = date_object.date()
-                print(f"The date you entered is: {date}") 
-                break
-            except ValueError:
-                print("Please enter in YYYY-MM-DD")
-        expenses["Amount"].append(amount)
-        expenses["Category"].append(category)
-        expenses["Name"].append(name)
-        expenses["Date"].append(date)
-        print(f"{expenses}")
-        
-def view_expenses():
-    if len(expenses) < 1:
-        print("Expense Tracker - Current Expenses:\nNo expenses found!\n") 
-        user_input = input("Press enter to Exit: ")
-        if user_input.lower() == 'exit':
-            clear()
-            return
-        else:
-            clear()
-            print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
-            main()
-            return
-    else:
-        print("Expense Tracker - Current Expenses:\n")
-        print(expenses)
-        user_input = input("Press enter to Exit: ")
-        if user_input.lower() == 'exit':
-            clear()
-            return
-        else:
-            clear()
-            print("Action:\n1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Delete Expenses\n\n\n")
-            main()
-            return
+        main("Invalid action, please try again.")
 
-def calculate_total_expenses():
-    print("")
+def add_expenses():
+    print("Expense Tracker\n")
+    name = input("Enter a name for your expense: ")
+    category = input("Enter a category for your expense: ")
+    while True:
+        try:
+            amount = input("Enter an amount: ")
+            amount_formatted = "{:.2f}".format(float(amount))
+            break
+        except:
+            clear()
+            print("Please enter a valid number")
+            time.sleep(1)
+            clear()
+            print(f"Expense Tracker\n\nEnter a name for your expense: {name}\nEnter a category for your expense: {category}")
+    while True:
+        date_input = input("Enter a date (in YYYY-MM-DD format): ")
+        try:
+            date_object = datetime.strptime(date_input, "%Y-%m-%d")
+            date = date_object.date()
+            break
+        except:
+            clear()
+            print("Please enter in YYYY-MM-DD format")
+            time.sleep(1)
+            clear()
+            print(f"Expense Tracker\n\nEnter a name for your expense: {name}\nEnter a category for your expense: {category}\nEnter an amount: {amount}")
+    expenses["Amount"].append(amount_formatted)
+    expenses["Category"].append(category)
+    expenses["Name"].append(name)
+    expenses["Date"].append(date)
+    print(f"\nExpense added")
 
 def delete_expenses():
     print("")
 
-main()
+main("")
